e0cb396c480c7c9ce86fd1a204fba1094fbefd11
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-10-09 21:42:06 +0200

    Update dog.tsx

--- pages/dog.tsx
+++ pages/dog.tsx
@@ -12,10 +12,10 @@ export default function DogPage() {
   return (
     <div>
       <Head>
-        <title>The Dog</title>
+        <title>The Mobile Blog</title>
       </Head>
-      <h1>The Dog (Hybrid AMP Page)</h1>
-      <Byline author="Meow Meow Fuzzyface" />
+      <h1>The Mobile Web Privacy</h1>
+      <Byline author="Don't Sell My Info" />
       <p>
         <a href={isAmp ? '/dog' : '/dog?amp=1'}>
           {isAmp ? 'View Non-AMP' : 'View AMP'} Version
@@ -36,59 +36,7 @@ export default function DogPage() {
         carrot cake muffin tiramisu lemon drops liquorice topping brownie.
         Soufflé chocolate cake croissant cupcake jelly.
       </p>
-      <p>
-        Muffin gummies dessert cheesecake candy canes. Candy canes danish cotton
-        candy tart dessert powder bear claw marshmallow. Muffin chocolate
-        marshmallow danish. Chocolate bar biscuit cake tiramisu. Topping sweet
-        brownie jujubes powder marzipan. Croissant wafer bonbon chupa chups cake
-        cake marzipan caramels jujubes. Cupcake cheesecake sweet roll
-        marshmallow lollipop danish jujubes jelly icing. Apple pie chupa chups
-        lollipop jelly-o cheesecake jelly beans cake dessert. Tootsie roll
-        tootsie roll bonbon pastry croissant gummi bears cake cake. Fruitcake
-        sugar plum halvah gingerbread cookie pastry chupa chups wafer lemon
-        drops. Marshmallow liquorice oat cake lollipop. Lemon drops oat cake
-        halvah liquorice danish powder cupcake soufflé. Cake tart topping
-        jelly-o tart sugar plum. Chocolate bar cookie wafer tootsie roll candy
-        cotton candy toffee pie donut.
-      </p>
-      <p>
-        Ice cream lollipop marshmallow tiramisu jujubes croissant. Bear claw
-        lemon drops marzipan candy bonbon cupcake powder. Candy canes cheesecake
-        bear claw pastry cake donut jujubes. Icing tart jelly-o soufflé bonbon
-        apple pie. Cheesecake pie chupa chups toffee powder. Bonbon lemon drops
-        carrot cake pudding candy halvah cheesecake lollipop cupcake. Pudding
-        marshmallow fruitcake. Gummi bears bonbon chupa chups lemon drops. Wafer
-        dessert gummies gummi bears biscuit donut tiramisu gummi bears brownie.
-        Tootsie roll liquorice bonbon cookie. Sesame snaps chocolate bar cake
-        croissant chupa chups cheesecake gingerbread tiramisu jelly. Cheesecake
-        ice cream muffin lollipop gummies. Sesame snaps jelly beans sweet bear
-        claw tart.
-      </p>
-      <p>
-        Sweet topping chupa chups chocolate cake jelly-o liquorice danish.
-        Pastry jelly beans apple pie dessert pastry lemon drops marzipan
-        gummies. Jelly beans macaroon bear claw cotton candy. Toffee sweet
-        lollipop toffee oat cake. Jelly-o oat cake fruitcake chocolate bar
-        sweet. Lemon drops gummies chocolate cake lollipop bear claw croissant
-        danish icing. Chocolate bar donut brownie chocolate cake lemon drops
-        chocolate bar. Cake fruitcake pudding chocolate apple pie. Brownie
-        tiramisu chocolate macaroon lemon drops wafer soufflé jujubes icing.
-        Cheesecake tiramisu cake macaroon tart lollipop donut. Gummi bears
-        dragée pudding bear claw. Muffin cake cupcake candy canes. Soufflé candy
-        canes biscuit. Macaroon gummies danish.
-      </p>
-      <p>
-        Cupcake cupcake tart. Cotton candy danish candy canes oat cake ice cream
-        candy canes powder wafer. Chocolate sesame snaps oat cake dragée
-        cheesecake. Sesame snaps marshmallow topping liquorice cookie
-        marshmallow. Liquorice pudding chocolate bar. Cake powder brownie
-        fruitcake. Carrot cake dessert marzipan sugar plum cupcake cheesecake
-        pastry. Apple pie macaroon ice cream fruitcake apple pie cookie. Tootsie
-        roll ice cream oat cake cheesecake donut cheesecake bear claw. Sesame
-        snaps marzipan jelly beans chocolate tootsie roll. Chocolate bar donut
-        dragée ice cream biscuit. Pie candy canes muffin candy canes ice cream
-        tiramisu.
-      </p>
+      
     </div>
   )
 }
